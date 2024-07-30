9652c53d359e2fd2c94713274e6994eb847eae43
haeraam <nooroongy@dyphi.com>
2024-07-30 18:33:23 +0900

    [ADD] touch-action

--- app/ripple/page.tsx
+++ app/ripple/page.tsx
@@ -16,10 +16,10 @@ export default function RipplePage() {
           transition={{ duration: 1 }}
           className="fixed bottom-12 flex items-center justify-center gap-6 rounded-full bg-black bg-opacity-10 p-6"
         >
-          <DraggableCircle id="1" size={16} />
-          <DraggableCircle id="2" size={24} />
-          <DraggableCircle id="3" size={32} />
-          <DraggableCircle id="4" size={48} />
+          <DraggableCircle id="1" size={16} touch-action />
+          <DraggableCircle id="2" size={24} touch-action />
+          <DraggableCircle id="3" size={32} touch-action />
+          <DraggableCircle id="4" size={48} touch-action />
         </motion.div>
       </DndContext>
     </div>
