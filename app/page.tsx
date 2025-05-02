0030c13df850f68e837e25d17ab4fba44c69deb4
hetarho <sunlikeperson@gmail.com>
2025-05-02 10:37:15 +0900

    test: netlify deploy test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        quos.
+      </p>
+    </div>
+  );
 }
